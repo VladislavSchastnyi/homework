559ef5b0c61fdb3604f60c7b1f6a880617d11a83
VladislavSchastnyi <32384821+VladislavSchastnyi@users.noreply.github.com>
2017-10-06 23:44:13 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -11,187 +11,192 @@ int main(){
   int FirstNumber=1;
   char SecondString[256];
   int SecondNumber=2;
+  int Result=0;
   
-  cout << "Выберите операцию [+, -, *, /, %, ^, !, &, |, <, >]: ";
-  cin >> Action;
+  while(Result==0){
+    cout << "Выберите операцию [+, -, *, /, %, ^, !, &, |, <, >]: ";
+    cin >> Action;
 
-  switch (Action[0]){
+    switch (Action[0]){
     
-    case '+': {
+      case '+': {
       
-      cout << "Введите первое число:";
-      cin >> FirstString;
+        cout << "Введите первое число:";
+        cin >> FirstString;
 
-      cout << "Введите второе число:";
-      cin >> SecondString;
+        cout << "Введите второе число:";
+        cin >> SecondString;
  
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber+SecondNumber) << "\n";
-      break;
-    }
-
-    case '-': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber-SecondNumber) << "\n";
-      break;
-    }
-
-    case '*': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber*SecondNumber) << "\n";
-      break;
-    }
-
-    case '%': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber%SecondNumber) << "\n";
-      break;
-    }
-
-    case '|': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber|SecondNumber) << "\n";
-      break;
-    }
-
-    case '&': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber&SecondNumber) << "\n";
-      break;
-    }
-
-    case '!': {
-      
-      cout << "Введите число:";
-      cin >> FirstString;
-
-      FirstNumber=atoi(FirstString);
-      cout << Action[0] << FirstNumber << "=" << (!FirstNumber) << "\n";
-      break;
-    }
-
-    case '^': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      FirstNumber=atoi(FirstString);
-      SecondNumber=atoi(SecondString);
-      cout << FirstNumber << Action[0] << SecondNumber << "=" << (pow(FirstNumber, SecondNumber)) << "\n";
-      break;
-    }
-
-    case '<': {
-        
-      int x;
-
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите количество бит для сдвига:";
-      cin >> SecondString;
-       
-      FirstNumber=atoi(FirstString);
-      cout << FirstNumber;
-      x=FirstNumber;
-      SecondNumber=atoi(SecondString);
-      
-      x=x >> (32-SecondNumber); 
-      FirstNumber=FirstNumber << SecondNumber; 
-
-      cout << Action[0] << SecondNumber << "=" << (FirstNumber|x) << "\n";
-      break;
-    }
-
-    case '>': {
-        
-      int x;
-
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите количество бит для сдвига:";
-      cin >> SecondString;
-       
-      FirstNumber=atoi(FirstString);
-      cout << FirstNumber;
-      x=FirstNumber;
-      SecondNumber=atoi(SecondString);
-      
-      x=x << (32-SecondNumber); 
-      FirstNumber=FirstNumber >> SecondNumber; 
-
-      cout << Action[0] << SecondNumber << "=" << (FirstNumber|x) << "\n";
-      break;
-    }
-
-    case '/': {
-      
-      cout << "Введите первое число:";
-      cin >> FirstString;
-
-      cout << "Введите второе число:";
-      cin >> SecondString;
- 
-      SecondNumber=atoi(SecondString);
-      if (SecondNumber!=0){
         FirstNumber=atoi(FirstString);
-        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber/SecondNumber) << "\n";
-      }else{
-        cout << "Делить на ноль нельзя" << "\n";
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber+SecondNumber) << "\n";
+        break;
       }
-      break;
-    }
 
-    default:{
-      cout << "Некорректная операция " << "\n";
-      break;
+      case '-': {
+      
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        FirstNumber=atoi(FirstString);
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber-SecondNumber) << "\n";
+        break;
+      }
+
+      case '*': {
+       
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        FirstNumber=atoi(FirstString);
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber*SecondNumber) << "\n";
+        break;
+      }
+
+      case '%': {
+      
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        FirstNumber=atoi(FirstString);
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber%SecondNumber) << "\n";
+        break;
+      }
+
+      case '|': {
+      
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        FirstNumber=atoi(FirstString);
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber|SecondNumber) << "\n";
+        break;
+      }
+
+      case '&': {
+      
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        FirstNumber=atoi(FirstString);
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber&SecondNumber) << "\n";
+        break;
+      }
+
+      case '!': {
+      
+        cout << "Введите число:";
+        cin >> FirstString;
+
+        FirstNumber=atoi(FirstString);
+        cout << Action[0] << FirstNumber << "=" << (!FirstNumber) << "\n";
+        break;
+      }
+
+      case '^': {
+      
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        FirstNumber=atoi(FirstString);
+        SecondNumber=atoi(SecondString);
+        cout << FirstNumber << Action[0] << SecondNumber << "=" << (pow(FirstNumber, SecondNumber)) << "\n";
+        break;
+      }
+
+      case '<': {
+        
+        int x;
+
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите количество бит для сдвига:";
+        cin >> SecondString;
+       
+        FirstNumber=atoi(FirstString);
+        cout << FirstNumber;
+        x=FirstNumber;
+        SecondNumber=atoi(SecondString);
+      
+        x=x >> (32-SecondNumber); 
+        FirstNumber=FirstNumber << SecondNumber; 
+
+        cout << Action[0] << SecondNumber << "=" << (FirstNumber|x) << "\n";
+        break;
+      }
+
+      case '>': {
+         
+        int x;
+ 
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите количество бит для сдвига:";
+        cin >> SecondString;
+       
+        FirstNumber=atoi(FirstString);
+        cout << FirstNumber;
+        x=FirstNumber;
+        SecondNumber=atoi(SecondString);
+      
+        x=x << (32-SecondNumber); 
+        FirstNumber=FirstNumber >> SecondNumber; 
+
+        cout << Action[0] << SecondNumber << "=" << (FirstNumber|x) << "\n";
+        break;
+      }
+
+      case '/': {
+        
+        cout << "Введите первое число:";
+        cin >> FirstString;
+
+        cout << "Введите второе число:";
+        cin >> SecondString;
+ 
+        SecondNumber=atoi(SecondString);
+        if (SecondNumber!=0){
+          FirstNumber=atoi(FirstString);
+          cout << FirstNumber << Action[0] << SecondNumber << "=" << (FirstNumber/SecondNumber) << "\n";
+        }else{
+          cout << "Делить на ноль нельзя" << "\n";
+        }
+        break;
+      }
+
+      default:{
+        cout << "Некорректная операция " << "\n";
+        Result=1;
+        break;
+      }
     }
   }
   
   return 0; 
 }
+
