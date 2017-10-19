17798ce2dda596c19f884c72c6e0399969977e38
VladislavSchastnyi <32384821+VladislavSchastnyi@users.noreply.github.com>
2017-10-20 02:02:25 +0300

    Update homework2.cpp

--- homework2.cpp
+++ homework2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -18,7 +19,25 @@ int delenie(int a, int b) {
 	return a / b;
 }
 
+int modul(int a, int b) {
+    return a % b;
+}
 
+int stepen(int a, int b) {
+    return (pow(a, b));
+}
+
+int logici(int a, int b) {
+	return a & b;
+}
+
+int logicili(int a, int b) {
+	return a | b;
+}
+
+int logicnet(int a) {
+	return !a;
+}
 
 int main(int argc, char *argv[]) {
   char operation = 'c';
@@ -56,6 +75,39 @@ int main(int argc, char *argv[]) {
     cin >> b;
     cout << a << "/" << b << "=" <<  delenie( a,  b) << endl;
     break;
+ case '%': 
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << a << "%" << b << "=" << modul( a,  b) << endl;
+    break;
+ case '^': 
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << a << "^" << b << "=" << stepen( a,  b) << endl;
+    break;
+ case '&': 
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << a << "&" << b << "=" << logici( a,  b) << endl;
+    break;  
+ case '|': 
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << a << "&" << b << "=" << logicili( a,  b) << endl;
+    break;  
+ case '!': 
+    cout << "Введите число: ";
+    cin >> a;
+    cout << a << "=" << logicnet( a) << endl;
+    break; 
   }
 }
    return 0;
